1120cdebd3839354933bdc490a57f7f914914981
Tiffany Nguyen <tiffkwin@users.noreply.github.com>
2019-01-18 11:00:50 -0500

    Add files via upload

--- darwin/merge_h2o2.py
+++ darwin/merge_h2o2.py
@@ -15,54 +15,55 @@ import os
 import glob
 import pandas as pd
 
-root = os.getcwd()
-path = root + '/*.xlsx'
-files = glob.glob(path)  
+def main():
+	root = os.getcwd()
+	path = root + '/*.xlsx'
+	files = glob.glob(path)  
 
-# Creates output folder called "Merge"
-output_dir = root +'/Merge'
-if (os.path.isdir(output_dir) == False):
-	os.makedirs('Merge')
+	# Creates output folder called "Merge"
+	output_dir = root +'/Merge'
+	if (os.path.isdir(output_dir) == False):
+		os.makedirs('Merge')
 
-writer = pd.ExcelWriter('Merge.xlsx')
-dfs = {}
-mergedfs = {}
+	writer = pd.ExcelWriter('Merge.xlsx')
+	dfs = {}
+	mergedfs = {}
 
-print("Analyzing data...")
+	print("Analyzing data...")
 
-# Loops through each excel file
-for name in files:
+	# Loops through each excel file
+	for name in files:
 
-	# Saves each worksheet in the file as a dataframe
-	filename = name.split('/')[-1]
-	filename = filename.split('.')[0]
-	xl = pd.ExcelFile(name)
-	dfs[filename] = xl.parse("Corrected Standard Curve Slopes")
+		# Saves each worksheet in the file as a dataframe
+		filename = name.split('/')[-1]
+		filename = filename.split('.')[0]
+		xl = pd.ExcelFile(name)
+		dfs[filename] = xl.parse("Corrected Standard Curve Slopes")
 
-# Loops through each dataframe
-for df in dfs:
+	# Loops through each dataframe
+	for df in dfs:
 
-	# Merges dataframes and normalizes each column's 
-	for column_name in dfs[df].columns:
-		normalized_col_name = column_name.split(".")[0]
-		if normalized_col_name in mergedfs:
-			(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
-		else:
-			mergedfs[normalized_col_name] = pd.DataFrame()
-			(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
+		# Merges dataframes and normalizes each column's 
+		for column_name in dfs[df].columns:
+			normalized_col_name = column_name.split(".")[0]
+			if normalized_col_name in mergedfs:
+				(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
+			else:
+				mergedfs[normalized_col_name] = pd.DataFrame()
+				(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
 
-# Produces Mean and SEM across rows in merged dataframes
-for df in mergedfs:
-	x = len(mergedfs[df].columns)
-	(mergedfs[df])['Mean'] = mergedfs[df].mean(axis=1)
-	(mergedfs[df])['SEM'] = (mergedfs[df].drop('Mean', axis=1).std(axis=1)).div(math.sqrt(x))
+	# Produces Mean and SEM across rows in merged dataframes
+	for df in mergedfs:
+		x = len(mergedfs[df].columns)
+		(mergedfs[df])['Mean'] = mergedfs[df].mean(axis=1)
+		(mergedfs[df])['SEM'] = (mergedfs[df].drop('Mean', axis=1).std(axis=1)).div(math.sqrt(x))
 
-os.chdir(output_dir)
+	os.chdir(output_dir)
 
-# Exports merged dataframes to excel
-for df in mergedfs:
-	mergedfs[df].to_excel(writer,df.replace('/','_'))
+	# Exports merged dataframes to excel
+	for df in mergedfs:
+		mergedfs[df].to_excel(writer,df.replace('/','_'))
 
-# Saves excel file
-writer.save()
-print('Analysis complete')
+	# Saves excel file
+	writer.save()
+	print('Analysis complete')
--- darwin/merge_nadh.py
+++ darwin/merge_nadh.py
@@ -15,55 +15,56 @@ import os
 import glob
 import pandas as pd
 
-root = os.getcwd()
-path = root + '/*.xlsx'
-files = glob.glob(path)  
+def main():
+	root = os.getcwd()
+	path = root + '/*.xlsx'
+	files = glob.glob(path)  
 
-# Creates output folder called "Merge"
-output_dir = root +'/Merge'
-if (os.path.isdir(output_dir) == False):
-	os.makedirs('Merge')
+	# Creates output folder called "Merge"
+	output_dir = root +'/Merge'
+	if (os.path.isdir(output_dir) == False):
+		os.makedirs('Merge')
 
-writer = pd.ExcelWriter('Merge.xlsx')
-dfs = {}
-mergedfs = {}
+	writer = pd.ExcelWriter('Merge.xlsx')
+	dfs = {}
+	mergedfs = {}
 
-print("Analyzing data...")
+	print("Analyzing data...")
 
-# Loops through each excel file
-for name in files:
+	# Loops through each excel file
+	for name in files:
 
-	# Saves each worksheet in the file as a dataframe
-	print(name)
-	filename = name.split('/')[-1]
-	filename = filename.split('.')[0]
-	xl = pd.ExcelFile(name)
-	dfs[filename] = xl.parse("Averaged Reduced Data")
+		# Saves each worksheet in the file as a dataframe
+		print(name)
+		filename = name.split('/')[-1]
+		filename = filename.split('.')[0]
+		xl = pd.ExcelFile(name)
+		dfs[filename] = xl.parse("Averaged Reduced Data")
 
-# Loops through each dataframe
-for df in dfs:
+	# Loops through each dataframe
+	for df in dfs:
 
-	# Merges dataframes and normalizes each column's name
-	for column_name in dfs[df].columns:
-		normalized_col_name = column_name.split(".")[0]
-		if normalized_col_name in mergedfs:
-			(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
-		else:
-			mergedfs[normalized_col_name] = pd.DataFrame()
-			(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
+		# Merges dataframes and normalizes each column's name
+		for column_name in dfs[df].columns:
+			normalized_col_name = column_name.split(".")[0]
+			if normalized_col_name in mergedfs:
+				(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
+			else:
+				mergedfs[normalized_col_name] = pd.DataFrame()
+				(mergedfs[normalized_col_name])[df] = (dfs[df])[column_name]
 
-# Produces Mean and SEM across rows in merged dataframes
-for df in mergedfs:
-	(mergedfs[df])['Mean'] = mergedfs[df].mean(axis=1)
-	(mergedfs[df])['SEM'] = (mergedfs[df].drop('Mean', axis=1).std(axis=1)).div(math.sqrt(x))
+	# Produces Mean and SEM across rows in merged dataframes
+	for df in mergedfs:
+		(mergedfs[df])['Mean'] = mergedfs[df].mean(axis=1)
+		(mergedfs[df])['SEM'] = (mergedfs[df].drop('Mean', axis=1).std(axis=1)).div(math.sqrt(x))
 
-os.chdir(output_dir)
+	os.chdir(output_dir)
 
-# Exports merged dataframes to excel
-for df in mergedfs:
-	print(mergedfs[df])
-	mergedfs[df].to_excel(writer,df.replace('/','_'))
+	# Exports merged dataframes to excel
+	for df in mergedfs:
+		print(mergedfs[df])
+		mergedfs[df].to_excel(writer,df.replace('/','_'))
 
-# Saves excel file
-writer.save()
-print('Analysis complete')
+	# Saves excel file
+	writer.save()
+	print('Analysis complete')
