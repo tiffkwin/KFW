300a2677510b7b0ce63aaeec085b5ed0e87c5b74
Tiffany Nguyen <tiffkwin@users.noreply.github.com>
2019-02-14 13:08:53 -0500

    Add files via upload

--- kfwy/setup.py
+++ kfwy/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="kfwy",
-    version="0.0.12",
+    version="0.0.16",
     author="Tiffany Nguyen",
     description="A data analysis toolkit for the KFW lab (Windows)",
     long_description=long_description,
